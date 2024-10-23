19acc3e7f3d3c0af5a2245368364b6aa9426464e
BertramHansen <beha0007@stud.kea.dk>
2024-10-23 10:15:25 +0200

    movie virker

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
+
         String title = scanner.nextLine();
         String director = scanner.nextLine();
         int yearCreated = scanner.nextInt();
         boolean isInColor = scanner.nextBoolean();
         int lengthInMinutes = scanner.nextInt();
-        String genre = scanner.nextLine();
+        String genre = scanner.next();
 
         Movie movie = new Movie(title, director, yearCreated, isInColor, lengthInMinutes, genre);
 
