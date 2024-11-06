9150364764a4991173d287d45488dc7e5e6594af
gustav-hr <gure0001@stud.kea.dk>
2024-11-06 12:49:13 +0100

    Comparators + smidt ting i mapper så det er lidt lettere overskueligt (når det kommer til comparators)

---
+++ src/Comparators/DirectorComparator.java
@@ -0,0 +1,15 @@
+package Comparators;
+
+import Rest.Movie;
+
+import java.util.Comparator;
+
+public class DirectorComparator implements Comparator<Movie> {
+
+    @Override
+    public int compare(Movie o1, Movie o2) {
+        return o1.getDirector().compareTo(o2.getDirector());
+
+    }
+}
+
---
+++ src/Comparators/YearComparator.java
@@ -0,0 +1,20 @@
+package Comparators;
+
+import Rest.Movie;
+
+import java.util.Comparator;
+
+public class YearComparator implements Comparator<Movie> {
+
+    @Override
+    public int compare(Movie o1, Movie o2) {
+        if (o1.getYearCreated() > o2.getYearCreated()) {
+            return 1;
+        } else if
+        (o1.getYearCreated() < o2.getYearCreated()) {
+            return -1;
+        }
+        return 0;
+    }
+
+}
